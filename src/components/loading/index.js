a05184ea452971228c559bc6c59382998708fb0c
JacobABrennan <jacobabrennan@gmail.com>
2020-02-27 00:23:56 -0500

    Document Loading component

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -1,13 +1,20 @@
 
 
-//== Loading ===================================================================
+/*== Loading ===================================================================
+
+Exports a simple React component, Loading, for use whenever the UI cannot be
+displayed because data is loading. It renders as a small animated "throbber".
+It does not user any props.
+
+*/
 
 //-- Dependencies --------------------------------
 import React from 'react';
 import './loading.css';
 
 //-- Component & Export --------------------------
-export default function Loading(props) {
+export default function Loading() {
+    // Note: &nbsp; is a special HTML entity meaning "non-breaking space"
     return (
         <div className="loading">
             <div className="loading-spinner">
