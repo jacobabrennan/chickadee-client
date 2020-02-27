4fd75c34817fb0fefcb26bf566bac93fe6a0c317
JacobABrennan <jacobabrennan@gmail.com>
2020-02-27 00:36:49 -0500

    Document Post component

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,6 +1,18 @@
 
 
-//==============================================================================
+/*== Post ======================================================================
+
+Exports a single React component, Post. Posts are media objects authored by
+individual users. Posts display a body of text, and the author's name and
+portrait. In the future, authors will be able to attach other media to posts,
+which users can then choose to view. Possible media types include images, links,
+audio, and large bodies of text.
+
+Accepts the following props:
+    post(object): A post object, as defined in the GraphQL schema.
+    userContext(object): A userContext object, as defined in the GraphQL schema.
+
+*/
 
 //-- Dependencies --------------------------------
 import React from 'react';
@@ -13,7 +25,7 @@ import Portrait from '../portrait/index.js';
 import UserName from '../user_name/index.js';
 import './post.css';
 
-//------------------------------------------------
+//-- React Component -----------------------------
 export default function Post(props) {
     // NOTE: need default portraitUrl
     const routerHistory = useHistory();
