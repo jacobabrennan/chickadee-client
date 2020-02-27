a124dd73e1e7ddb891248f6764916fec68aaaadd
JacobABrennan <jacobabrennan@gmail.com>
2020-02-27 00:28:41 -0500

    Document Portrait component

--- src/components/portrait/index.js
+++ src/components/portrait/index.js
@@ -1,13 +1,21 @@
 
 
-//==============================================================================
+/*== Portrait ==================================================================
+
+Exports a single React component, Portrait, which renders an image of a user.
+Accepts the following props:
+    user(object): A user object, as defined in the GraphQL schema.
+    large(boolean)[optional]: Determines at which of two standards sizes the
+        image will be displayed. Defaults to "false" (small image).
+
+*/
 
 //-- Dependencies --------------------------------
 import React from 'react';
 import { URL_IMAGE_PORTRAIT_DEFAULT } from '../../utilities/constants.js';
 import './portrait.css';
 
-//------------------------------------------------
+//-- React Component -------------------------------
 export default function Portrait({user, large}) {
     return (
         <img
